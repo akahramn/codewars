c9e9106c58d7d2dbe79a6cb4b63760325b60f01d
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-24 23:53:44 +0300

    bouncing balls kata solution.

---
+++ src/main/java/kyu6/BouncingBall.java
@@ -0,0 +1,21 @@
+package kyu6;
+
+public class BouncingBall {
+
+    public static int bouncingBall(double h, double bounce, double window) {
+
+        if(h < 0 || bounce < 0 || bounce >= 1 || window > h ) {
+            return -1;
+        }
+
+        int count = 1;
+        double bounceHeight = h * bounce;
+
+        while( bounceHeight > window) {
+            count += 2;
+            bounceHeight = bounceHeight * bounce;
+        }
+
+        return count;
+    }
+}
