dd87be689823220d530f9a8d2706858a5cabed74
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-25 10:18:05 +0300

    Converting string to camel case kata solution.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import kyu6.BouncingBall;
+import kyu6.ConvertStringToCamelCase;
 
 public class Main {
     public static void main(String[] args) {
         BouncingBall bouncingBall = new BouncingBall();
         bouncingBall.calculate(3.0, 0.66, 1.5);
 
+        ConvertStringToCamelCase convertStringToCamelCase = new ConvertStringToCamelCase();
 
+        String str = "the-stealth-warrior";
+        String str1 = "The_Stealth_Warrior";
+        convertStringToCamelCase.toCamelCase(str);
 
 
     }
