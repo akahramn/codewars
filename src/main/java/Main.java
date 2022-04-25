6afa151bed98489c27239f2996f93b715d70c199
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-25 10:12:24 +0300

    Converting string to camel case kata solution.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ import kyu6.BouncingBall;
 public class Main {
     public static void main(String[] args) {
         BouncingBall bouncingBall = new BouncingBall();
-
         bouncingBall.calculate(3.0, 0.66, 1.5);
+
+
+
+
     }
 }
---
+++ src/main/java/kyu6/ConvertStringToCamelCase.java
@@ -0,0 +1,20 @@
+package kyu6;
+
+public class ConvertStringToCamelCase {
+
+    public String toCamelCase(String s){
+
+        char[] arr = s.toCharArray();
+
+        for(int i = 1; i<arr.length; i++) {
+            if(arr[i-1] == '-' || arr[i-1] == '_' ) {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+        }
+        String result = new String(arr);
+        result = result.replace("-","");
+        result = result.replace("_","");
+
+        return result;
+    }
+}
