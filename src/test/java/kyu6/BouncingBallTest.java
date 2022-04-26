09ab2840156d1fb2b9bd973d08126325228a7118
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-04-27 00:20:17 +0300

    givenInputVariables_whenCalculate_thenReturnBouncing test method add.

--- src/test/java/kyu6/BouncingBallTest.java
+++ src/test/java/kyu6/BouncingBallTest.java
@@ -1,11 +1,16 @@
 package kyu6;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BouncingBallTest {
 
     @Test
     public void givenInputVariables_whenCalculate_thenReturnBouncing() {
+        BouncingBall bouncingBall = new BouncingBall();
+        int actualValue = bouncingBall.calculate(3.0, 0.66, 1.5);
+        int expectedValue = 3;
+        Assertions.assertEquals(expectedValue, actualValue);
 
     }
 }
